e4d030b5b505274fd90233cf4a1c415b5bedbc25
Raunak Poddar <raunak.poddar@gmail.com>
2012-08-21 11:15:54 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squitter::Application.config.secret_token = '724b6db9311934786386ea86e7d86c50daea372bc4ad82dc4ecfe12e5cdc210329142155c677e8837a3e419ca098b95ccdccc409beee2577c32cc8370f41dc29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Squitter::Application.config.session_store :cookie_store, key: '_squitter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Squitter::Application.config.session_store :active_record_store
